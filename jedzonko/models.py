aaddb87b90e0e5e32ab88d74da3c436dff80c522
Wojciech Mzyk <W_Mzyk@interia.pl>
2019-11-20 21:09:07 +0100

    Utworzono modele 5.1

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from enum import Enum
 
 
 # Create your models here.
@@ -11,3 +12,26 @@ class Recipe(models.Model):
     preparation_time = models.IntegerField(null=True)
     votes = models.IntegerField(default=0)
 
+class Plan(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    recipes = models.ManyToManyField(Recipe, through="RecipePlan")
+
+class DayChoices(Enum):
+    Mon = "Monday"
+    Tue = "Tuesday"
+    Wed = "Wednesday"
+    Thu = "Thursday"
+    Fri = "Friday"
+    Sat = "Saturday"
+    Sun = "Sunday"
+
+class DayName(models.Model):
+    name = models.CharField(max_length=5, choices=[(tag, tag.value) for tag in DayChoices])  # To use it just type DayChoices.<day> (eg. DayChoices.Sat)
+
+class RecipePlan(models.Model):
+    meal_name = models.CharField(max_length=24)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    day_name = models.ForeignKey(DayName, on_delete=models.CASCADE)
