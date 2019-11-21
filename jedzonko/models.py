ca2b5fd020b726519079b5109e42c1590d973b97
Wojciech Mzyk <W_Mzyk@interia.pl>
2019-11-22 00:06:36 +0100

    poprawki

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from enum import Enum
+from datetime import datetime
 
 
 # Create your models here.
@@ -8,7 +9,7 @@ class Recipe(models.Model):
     ingredients = models.CharField(max_length=64)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     preparation_time = models.IntegerField(null=True)
     votes = models.IntegerField(default=0)
 
@@ -30,8 +31,15 @@ class DayChoices(Enum):
 class DayName(models.Model):
     name = models.CharField(max_length=5, choices=[(tag, tag.value) for tag in DayChoices])  # To use it just type DayChoices.<day> (eg. DayChoices.Sat)
 
+class MealNames(Enum):
+    Breakfast = "Breakfast"
+    Lunch = "Lunch"
+    Dinner = "Dinner"
+    Tea = "Tea"
+    Supper = "Supper"
+
 class RecipePlan(models.Model):
-    meal_name = models.CharField(max_length=24)
+    meal_name = models.CharField(max_length=24, choices=[(tag, tag.value) for tag in MealNames])
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
     day_name = models.ForeignKey(DayName, on_delete=models.CASCADE)
